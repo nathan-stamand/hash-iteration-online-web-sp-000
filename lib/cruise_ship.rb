0abe69840585b4153559afa2c9f055b124d2ac53
Nathan St.Amand <nathansaint810@gmail.com>
2020-02-20 17:03:03 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,7 +8,7 @@ suite_e: "Crumpet the Elf"
 
 def select_winner(passengers)
   passengers.each do |key, value|
-    if key == suite_a: && value[0] == "A"
+    if key == suite_a && value[0] == "A"
       return value
     end
   end
